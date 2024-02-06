a6728047974a2b3c2db8b90f853da6b9f85ba1f6
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-02-06 15:50:36 -0500

    Added movement to the ghost

--- GhostGame/main.js
+++ GhostGame/main.js
@@ -11,6 +11,9 @@ let ghostX = bkgWidth/2 - ghostWidth/2;
 let ghostY = bkgHeight*7/8 - ghostHeight;
 let ghostImg;
 
+//movements
+let velocityX = 0;
+
 let ghost = {
     img : null,
     x : ghostX,
@@ -23,7 +26,7 @@ window.onload = function(){
     bkg = document.getElementById("bkg");
     bkg.height = bkgHeight;
     bkg.width = bkgWidth;
-    context = bkg.getContext("2d"); //used for drawing on the board
+    context = bkg.getContext("2d"); //used for drawing on background
 
     //add ghost
     ghostImg = new Image();
@@ -31,5 +34,32 @@ window.onload = function(){
     ghost.img = ghostImg;
     ghostImg.onload = function(){
         context.drawImage(ghost.img, ghost.x, ghost.y, ghost.width, ghost.height);
+    }
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveGhost);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, bkg.width, bkg.height);
+
+    //ghost
+    ghost.x += velocityX;
+    if (ghost.x > bkgWidth) {
+        ghost.x = 0; 
+    }else if (ghost.x + ghost.width < 0) {
+        ghost.x = bkgWidth;
+    }
+    context.drawImage(ghost.img, ghost.x, ghost.y, ghost.width, ghost.height);
+
+}
+
+function moveGhost(e) {
+    if (e.code == "ArrowRight") { //move right
+        velocityX = 4;
+    }
+    else if (e.code == "ArrowLeft") { //move left
+        velocityX = -4;
     }
 }
